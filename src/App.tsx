db587fe6deb226026c98dcc265fb86635f8f9a8d
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-06-26 10:16:22 +0900

    api取得データ（user）の型定義

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import "./styles.css";
 import { UserCard } from "./components/UserCard";
+import axios from "axios";
+import { User } from "./types/api/user";
+import { useState } from "react";
 
 const user = {
   id: 1,
@@ -7,9 +10,14 @@ const user = {
   email: "123@aaa.com",
   address: "ADRESS"
 };
+
 export default function App() {
+  const onClickFetchUser = () => {
+    axios.get<Array<User>>("https://jsonplaceholder.typicode.com/users");
+  };
   return (
     <div className="App">
+      <button onClick={onClickFetchUser}>データ取得</button>
       <UserCard user={user} />
     </div>
   );
