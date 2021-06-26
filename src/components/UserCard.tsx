8be68d1882295237da2ea7861c8b098ccd0d5e7d
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-06-26 10:03:32 +0900

    UserCardのCSS調整

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -6,9 +6,15 @@ type Props = {
 };
 
 export const UserCard: VFC<Props> = (props) => {
+  const style = {
+    border: "solid 1px #ccc",
+    borderRadius: "8px",
+    padding: "0 16px",
+    margin: "8px"
+  };
   const { user } = props;
   return (
-    <div>
+    <div style={style}>
       <dt>名前</dt>
       <dd>{user.name}</dd>
       <dt>メール</dt>
