09c5c08f5c52d1e7d665c35e2a24b8d6ec310e8b
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-06-26 10:31:45 +0900

    api取得データをUserCardに引き渡して表示

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { UserCard } from "./components/UserCard";
 import axios from "axios";
 import { User } from "./types/api/user";
 import { useState } from "react";
+import { UserProfile } from "./types/userProfiles";
 
 const user = {
   id: 1,
@@ -12,13 +13,26 @@ const user = {
 };
 
 export default function App() {
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
   const onClickFetchUser = () => {
-    axios.get<Array<User>>("https://jsonplaceholder.typicode.com/users");
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`
+        }));
+        setUserProfiles(data);
+      });
   };
   return (
     <div className="App">
       <button onClick={onClickFetchUser}>データ取得</button>
-      <UserCard user={user} />
+      {userProfiles.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
     </div>
   );
 }
